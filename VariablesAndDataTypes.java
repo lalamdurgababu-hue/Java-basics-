245bd4e2c4d2722c1814befd668d3e5b7e86a18d
Lalam Durga Babu <lalamdurgababu@gmail.com>
2026-07-18 21:00:41 +0530

    Add VariablesAndDataTypes.java

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,15 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        int age = 20;
+        float height = 5.8f;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Durga Babu";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
